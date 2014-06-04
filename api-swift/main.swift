7979204d063b942c4d3658527db67cadb1690d75
Eric Song <eric.song@rutgers.edu>
2014-06-04 14:08:00 -0400

    added new Agent code and a test function to main

--- api-swift/main.swift
+++ api-swift/main.swift
@@ -1,12 +1,35 @@
 //
 //  main.swift
-//  api-swift
+//  ordrin-api
 //
-//  Created by Sam Agnew on 6/3/14.
-//  Copyright (c) 2014 Sam Agnew. All rights reserved.
+//  Created by Eric Song on 6/4/14.
+//  Copyright (c) 2014 ordrin. All rights reserved.
 //
 
 import Foundation
 
-Ordrin.restaurant_details("147")
-println("Hey")
+func waitFor (inout wait: Bool) {
+    while (wait) {
+        NSRunLoop.currentRunLoop().runMode(NSDefaultRunLoopMode, beforeDate: NSDate(timeIntervalSinceNow: 0.1))
+    }
+}
+    
+    var wait: Bool = true
+
+    Agent.get("https://r-test.ordr.in/rd/147?_auth=1,8l3kW3pv2UZXOebdQ-YU9qoUeE8GPPzj7_We-WxbKek",
+        done: { (error: NSError?, response: NSHTTPURLResponse?, data: NSData?) -> () in
+  
+            if (error) {
+                println("error 2")
+                return
+            } else {
+                var results = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) as NSDictionary
+                
+                println(results["addr"])
+                wait = false
+            }
+        })
+   
+    waitFor(&wait)
+
+println("done")
